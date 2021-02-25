dd5580ad2baf7f3e6c812d4139ab91ecc2470f48
orhanbalci <orhanbalci@gmail.com>
2021-02-25 23:40:50 +0300

    rust install scripts added

---
+++ .config/install/rust.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+pamac install curl
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+cargo install bat
+cargo install fd-find
+cargo install git-delta
+cargo install navi
+cargo install oha
+cargo install ripgrep
+cargo install rsfetch
+cargo install starship
+cargo install tealdeer
+cargo install the-way
+cargo install vivid
+cargo install websocat
